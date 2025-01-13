85e0b3722f6ffa79a83c4da7c43493ea13c9d26c
jun5-soodal <j01024749503@gmail.com>
2025-01-13 13:19:33 +0900

    feat:lv3 기능개발

---
+++ src/main/java/lv3/Main.java
@@ -0,0 +1,17 @@
+package lv3;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Kiosk kiosk = new Kiosk(new ArrayList<>());
+        kiosk.start();
+
+    }
+
+
+}
